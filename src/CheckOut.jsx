6910d27bf3bc3106826a015194856b5d6c1d9ba8
Muhammad Ali <mali146643@gmail.com>
2024-11-13 16:34:34 +0500

    ready

--- src/CheckOut.jsx
+++ src/CheckOut.jsx
@@ -17,7 +17,7 @@ export default function CheckOut() {
 
     useEffect(() => {
         // This is where you send the cart total to your backend
-        fetch('https://eb-2.vercel.app/create-payment-intent', {
+        fetch('https://part2-fawn.vercel.app//create-payment-intent', {
           method: 'POST',
           body: JSON.stringify({ amount:total*100 }), // Example: Pass the amount in cents (e.g., $20.00)
           headers: {
